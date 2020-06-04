bbafe2ea28d39ebd97a5e6b57348b96c2dcbe706
Noel Bao <noel@fstk.io>
2020-06-04 10:10:26 +0800

    add root dir

--- src/utils/mode/pub_tag.js
+++ src/utils/mode/pub_tag.js
@@ -85,53 +85,47 @@ async function check(tags_from_csv, db_tag, db_pub, master_wallet) {
 }
 
 function modePubAndTag() {
-  level(
-    path.resolve(__dirname, "..", "..", "..", "level_db_pub"),
-    (err, db_pub) => {
+  level(path.resolve(argv.root_dir, "level_db_pub"), (err, db_pub) => {
+    if (err) throw err;
+
+    level(path.resolve(argv.root_dir, "level_db_tag"), (err, db_tag) => {
       if (err) throw err;
 
-      level(
-        path.resolve(__dirname, "..", "..", "..", "level_db_tag"),
-        (err, db_tag) => {
-          if (err) throw err;
+      let master_pk = null;
 
-          let master_pk = null;
+      try {
+        const file_master_pk_json = fs.readFileSync(
+          path.resolve(argv.root_dir, "master_pk")
+        );
+        master_pk = JSON.parse(file_master_pk_json).master_pk;
+      } catch (err) {
+        consola.error(err);
+      }
 
-          try {
-            const file_master_pk_json = fs.readFileSync(
-              path.resolve(__dirname, "..", "..", "..", "master_pk")
-            );
-            master_pk = JSON.parse(file_master_pk_json).master_pk;
-          } catch (err) {
-            consola.error(err);
-          }
-
-          if (master_pk === null) {
-            if (process.env.MASTER_PK_HEX_STR === undefined) {
-              consola.error("no MASTER_PK_HEX_STR env is assigned");
-              process.exit(1);
-            } else {
-              master_pk = process.env.MASTER_PK_HEX_STR;
-            }
-          }
-
-          const master_wallet = new Wallet(master_pk);
-
-          consola.success("Master wallet address:", master_wallet.address);
-
-          const tags = [];
-          fs.createReadStream(argv.tags)
-            .pipe(stripBom())
-            .pipe(csv())
-            .on("data", (data) => tags.push(data))
-            .on("end", () => {
-              consola.success("Tags are loaded");
-              check(tags, db_tag, db_pub, master_wallet);
-            });
+      if (master_pk === null) {
+        if (process.env.MASTER_PK_HEX_STR === undefined) {
+          consola.error("no MASTER_PK_HEX_STR env is assigned");
+          process.exit(1);
+        } else {
+          master_pk = process.env.MASTER_PK_HEX_STR;
         }
-      );
-    }
-  );
+      }
+
+      const master_wallet = new Wallet(master_pk);
+
+      consola.success("Master wallet address:", master_wallet.address);
+
+      const tags = [];
+      fs.createReadStream(argv.tags)
+        .pipe(stripBom())
+        .pipe(csv())
+        .on("data", (data) => tags.push(data))
+        .on("end", () => {
+          consola.success("Tags are loaded");
+          check(tags, db_tag, db_pub, master_wallet);
+        });
+    });
+  });
 }
 
 module.exports = { modePubAndTag };
